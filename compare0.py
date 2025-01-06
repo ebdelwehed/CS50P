0c71984e9604acd939419ada8e70c9706f814e2b
ebdelweheh <abdulwahid1@gmail.com>
2025-01-06 16:25:37 +0300

    adding new files

---
+++ compare0.py
@@ -0,0 +1,4 @@
+#barnaamij yaa weyn
+magac = input("cunuga magaciisa")
+if magac == maxamed:
+    print(curad)
---
+++ practice2/compare11.py
@@ -0,0 +1,5 @@
+answer = input("Do you agree ?")
+if answer.startswith("y"):
+    print("Agreed")
+else:
+    print("Not agreed")
